fe4fa5df87c40e0f28be941914272e003ac8aad1
GiovannaCardins <106175075+GiovannaCardins@users.noreply.github.com>
2022-11-15 15:24:30 -0300

    Add files via upload

---
+++ Atividade 1/src/code/Calculadora.py
@@ -0,0 +1,20 @@
+itens = """
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão 
+"""
+
+i = 's'
+while (i == 's'):
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+
+    print(itens)
+
+    escolha = int(input("Qual operação você gostaria de efetuar? "))
+
+        
+    
+
+    i = input("Quer continuar calculando? (s/n)")
